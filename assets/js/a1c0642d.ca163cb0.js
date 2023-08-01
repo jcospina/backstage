3d88bd372ac186ed118999c0e19cb772cc723fef
jcospina <juan.ospina.quintero@gmail.com>
2023-08-01 17:31:17 +0000

    Deploying to gh-pages from @ jcospina/backstage-fork@5623a818b957b0ef3d44434cd4b8afbf9ae43f7d 🚀

---
+++ assets/js/a1c0642d.ca163cb0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbackstage_microsite=self.webpackChunkbackstage_microsite||[]).push([[458527],{924469:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
